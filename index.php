abad92eb788153a735f8cf7b0d82483bb980d499
Luiscarlos04 <69157952+Luiscarlos04@users.noreply.github.com>
2020-08-13 16:04:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mr. Moncayo's 2020-2021 5th Grade Class</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <form method="post">
+    	<h1>sign in !</h1>
+    	<input type="text" name="name" placeholder="Full Name">
+    	<input type="text" name="email" placeholder="Homeroom Teacher">
+    	<input type="submit" name="register">
+    </form>
+        <?php
+        include("registrar.php");
+        ?>
+</body>
+</html>
